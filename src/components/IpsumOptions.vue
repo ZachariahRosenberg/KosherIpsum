6c891a17e218af6a85a0ce9d9600130c1d48f563
ZachariahRosenberg <zachariah.rosenberg@gmail.com>
2021-04-01 11:24:33 -0400

    added state for pargraph length and number of paragraphs in ipsumOptions and a validator for number of paragraphs

--- src/components/IpsumOptions.vue
+++ src/components/IpsumOptions.vue
@@ -1,15 +1,41 @@
 <template>
     <div class='options'>
+        <div v-if="showError">
+            {{ errorMessage }}
+        </div>
         <div class='row'>
-            <input type="number" id="quantity" name="quantity" min="1" max="5" />
+            <input 
+                type="number" 
+                id="quantity" 
+                name="quantity" 
+                min="1" 
+                max="5" 
+                v-model="ipsumNumParagraphs"
+                @change="validateNumParagraphs"
+            />
             <p>Paragraphs</p>
         </div>
         <div class='row'>
-            <input type="radio" id="radioLong" />
+            <input  
+                type="radio" 
+                id="radioLong" 
+                value = 'l'
+                v-model = 'ipsumLength'
+            />
             <p>Long</p>
-            <input type="radio" id="radioMedium" />
+            <input  
+                type="radio" 
+                id="radioMedium" 
+                value = 'm'
+                v-model = 'ipsumLength'
+            />
             <p>Medium</p>
-            <input type="radio" id="radioShort" />
+            <input  
+                type="radio" 
+                id="radioShort" 
+                value = 's'
+                v-model = 'ipsumLength'
+            />
             <p>Short</p>
         </div>
         <div class='row'>
@@ -33,11 +59,39 @@ export default {
     methods: {
         setOutputText: function(){
             this.$emit('set-output-text', this.ipsumText);
+        },
+
+        setIpsumLength: function(length){
+            if(length in ['s', 'm', 'l']){
+                this.ipsumLength = length;
+            }
+        },
+
+        validateNumParagraphs: function(){
+            this.errorMessage = "";
+            this.showError = false;
+
+            if(this.ipsumNumParagraphs > 5){
+                this.ipsumNumParagraphs = 5;
+                this.errorMessage = 'Maximum number of paragraphs is 5';
+                this.showError = true
+            }
+            else if(this.ipsumNumParagraphs < 1){
+                this.ipsumNumParagraphs = 1;
+                this.errorMessage = 'Minimum number of paragraphs is 1';
+                this.showError = true
+            }
         }
     },
     data(){
         return {
-            ipsumText: ""
+            ipsumText         : "",
+            ipsumLength       : "s",
+            ipsumNumParagraphs: 3,
+            ipsumOptionOne    : false,
+            ipsumOptionTwo    : false,
+            errorMessage      : "",
+            showError         : false
         }
     }
 }
