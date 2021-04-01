6f61cabaef67c2fad8d520b380a893d6ac46712b
ZachariahRosenberg <zachariah.rosenberg@gmail.com>
2021-04-01 11:06:42 -0400

    Refactored home component and split options and output text into child components. Added button to toggledisplay of output text on btn generate

---
+++ src/components/IpsumOptions.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class='options'>
+        <div class='row'>
+            <input type="number" id="quantity" name="quantity" min="1" max="5" />
+            <p>Paragraphs</p>
+        </div>
+        <div class='row'>
+            <input type="radio" id="radioLong" />
+            <p>Long</p>
+            <input type="radio" id="radioMedium" />
+            <p>Medium</p>
+            <input type="radio" id="radioShort" />
+            <p>Short</p>
+        </div>
+        <div class='row'>
+            <input type="checkbox" id='checkOne' />
+            <p>This is option "one"</p>
+        </div>
+        <div class='row'>
+            <input type="checkbox" id='checkTwo' />
+            <p>This is option "two"</p>
+        </div>
+    </div>
+
+    <button type="button" @click="setOutputText">
+        <span>Generate</span>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'IpsumOptions',
+    methods: {
+        setOutputText: function(){
+            this.$emit('set-output-text', this.ipsumText);
+        }
+    },
+    data(){
+        return {
+            ipsumText: ""
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+    .options {
+        margin: auto;
+        width: 50%;
+    }
+
+    .row {
+        display: flex;
+        align-items: baseline;
+
+        > * {
+            margin: 5px
+        }
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
         <div class="container">
-
+            <!-- TITLE -->
             <div class="gutter">gutter</div>
             <div class="section">
                 <h1 class="primary-font">Kosher Ipsum</h1>
@@ -9,47 +9,24 @@
             </div>
             <div class="gutter2">gutter</div>
 
+            <!-- Catchline -->
             <div class="section">
                 <h2 class="primary-font">
-                    This is our large header
-                    <br />
-                    Text? Don’t you love it?
+                    So you want a shot of pshat?
                 </h2>
                 <h4 class="secondary-font">
-                    I know Right?
-                    It's mamash the best...
+                    So you think you have some hotshot website that needs a bissell yiddish?  Well bubbeleh, a gut glik to you.
                 </h4>
             </div>
 
+            <!-- Options -->
             <div class="section">
-                <div class='options'>
-                    <div class='row'>
-                        <input type="number" id="quantity" name="quantity" min="1" max="5" />
-                        <p>Paragraphs</p>
-                    </div>
-                    <div class='row'>
-                        <input type="radio" id="radioLong" />
-                        <p>Long</p>
-                        <input type="radio" id="radioMedium" />
-                        <p>Medium</p>
-                        <input type="radio" id="radioShort" />
-                        <p>Short</p>
-                    </div>
-                    <div class='row'>
-                        <input type="checkbox" id='checkOne' />
-                        <p>This is option "one"</p>
-                    </div>
-                    <div class='row'>
-                        <input type="checkbox" id='checkTwo' />
-                        <p>This is option "two"</p>
-                    </div>
-                </div>
+                <IpsumOptions @set-output-text='setOutputText' />
             </div>
 
-            <div class='section'>
-                <button type="button">
-                    <span>Generate</span>
-                </button>
+            <!-- GENERATE TEXT -->
+            <div class='section' v-if="showOutput">
+                <ipsum-output></ipsum-output>
             </div>
 
         </div>
@@ -58,34 +35,38 @@
 
 <script>
     // @ is an alias to /src
-    import HelloWorld from '@/components/HelloWorld.vue'
+    import IpsumOptions from '@/components/IpsumOptions';
+    import IpsumOutput  from '@/components/IpsumOutput';
 
     export default {
         name: 'Home',
+
         components: {
-            HelloWorld
+            IpsumOptions,
+            IpsumOutput
+        },
+
+        methods: {
+            setOutputText(text){
+                this.outputText = text;
+                this.showOutput = true;
+            }
+        },
+
+        data() {
+            return {
+                showOutput: false,
+                outputText: ""
+            }
+        },
+
+        computed: {
+
         }
     }
 </script>
 
-<style lang="scss" scoped>
-    .home {
-        background-color: #3c78d8ff;
-        color: white;
-        font-family: 'Akaya Kanadaka', cursive;
-        font-size: 1.5em;
-    }
-    .primary-font {
-        color: #ffd966;
-    }
-    .secondary-font {
-        color: white;
-    }
-    .container {
-        display: grid;
-        grid-template-columns: [gutter] 10% [main] auto [gutter2] 10%;
-        grid-template-areas: "gutter main gutter2";
-    }
+<style lang="scss">
 
     h1 {
         font-size: 4em;
@@ -100,34 +81,6 @@
     h4 {
         font-size: 1.5em;
         margin: 10px;
-    }
-
-    .section {
-        grid-column-start: main;
-        margin: 25px;
-    }
-
-    .options {
-        margin: auto;
-        width: 50%;
-    }
-
-    .gutter {
-        grid-area: gutter;
-        background-color: red;
-    }
-    .gutter2 {
-        grid-area: gutter2;
-        background-color: red;
-    }
-
-    .row {
-        display: flex;
-        align-items: baseline;
-
-        > * {
-            margin: 5px
-        }
     }
 
     button {
@@ -189,5 +142,38 @@
             
             transition: width .2s ease-out, padding-top .2s ease-out;
         }
+    }
+
+    .home {
+        background-color: #3c78d8ff;
+        color: white;
+        font-family: 'Akaya Kanadaka', cursive;
+        font-size: 1.5em;
+    }
+    .primary-font {
+        color: #ffd966;
+    }
+    .secondary-font {
+        color: white;
+    }
+
+    .container {
+        display: grid;
+        grid-template-columns: [gutter] 10% [main] auto [gutter2] 10%;
+        grid-template-areas: "gutter main gutter2";
+    }
+
+    .section {
+        grid-column-start: main;
+        margin: 25px;
+    }
+
+    .gutter {
+        grid-area: gutter;
+        background-color: red;
+    }
+    .gutter2 {
+        grid-area: gutter2;
+        background-color: red;
     }
 </style>
