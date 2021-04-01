912e1a55e889e1f0de1780f31dec4d250bd3e065
ZachariahRosenberg <zachariah.rosenberg@gmail.com>
2021-04-01 15:58:12 -0400

    have first cloud function working. also wrote some todos for what to do next  in teh comments

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,34 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.repeat = functions.https.onRequest(async (req, res)=>{
+    let text = req.query.text;
+
+    // Log request
+    functions.logger.info(`REPEAT: ${text}`);
+
+    res.json({
+        result: text.toUpperCase()
+    })
+});
+
+/*
+Receive:
+    ipsumNumParagraphs
+    ipsumLength
+    excludeIpsum
+    startWithOy
+
+Do:
+    Random generate paragraphs
+        Pull from list of phrases and words.
+        Create sentences with punctuation.
+        Mix in some Ipsum, if requested.
+    Ensure paragraph ends on word with punctuation
+
+Respond:
+    Array of paragraph strings
+*/
+
